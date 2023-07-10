513b5eb5745484b1e5198e548577df3b06e51481
fairuzhanifah1709 <fairuzhanifah1709@gmail.com>
2023-07-10 22:29:53 +0800

    frontend

---
+++ js/script.js
@@ -0,0 +1,29 @@
+function kirim(){
+    var isi = document.getElementById("send");
+    if(isi.value = "")
+    alert("Successfully sent" )
+    else{
+        alert("Please fill in")
+    }
+}
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+};
+var swiper = new Swiper(".review-slider", {
+    spaceBetween: 20,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    loop: true,
+    grabCursor: true,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    },
+);
